3f848d706c7a0a23f117d1b5b11de4f6ebde985d
adeeb boumoghlbey <adeebboumoghlbey@users.noreply.github.com>
2024-08-23 15:41:31 +0300

    fix function and variable names, and restructure playround()
    -variable and function names used three phrases (choice,value,guess), so
    they got renamed to all use "choice"
    
    -playround() didin't display the results at each round so
    displayRoundResult() handles that
    
    -playround() couldn't handle a tie so it was restructured to do so

--- javascript.js
+++ javascript.js
@@ -1,38 +1,50 @@
-function getComputerValue(){
-    let guess = Math.floor(Math.random()*3) + 1;
-    if(guess == 1)return "Rock";
-    else if(guess == 2)return "Paper";
+function getComputerChoice(){
+    let Choice = Math.floor(Math.random()*3) + 1;
+    if(Choice == 1)return "Rock";
+    else if(Choice == 2)return "Paper";
     else return "Scissors";
 }
 
-function validValue(s){
+function validChoice(s){
     return s == "Rock" || s == "Paper" || s == "Scissors";
 }
 
-function getHumanValue(){
-    let guess = "l";
-    while(! validValue(guess)){
-        guess = prompt("Enter your guess(Rock-Paper-Scissors):");
-        guess = guess.charAt(0).toUpperCase() + guess.slice(1).toLowerCase();
+function getHumanChoice(){
+    let Choice = "l";
+    while(! validChoice(Choice)){
+        Choice = prompt("Enter your Choice(Rock-Paper-Scissors):");
+        Choice = Choice.charAt(0).toUpperCase() + Choice.slice(1).toLowerCase();
     }
-    return guess;
+    return Choice;
 }
 
-function(){
-
-}
-function playRound(humanGuess,computerGuess){
-    if(humanGuess == "Rock"){
-        computerGuess == "Paper" ? computerScore++ : humanScore++;
-    }else if(humanGuess == "Paper"){
-        computerGuess == "Scissors" ? computerScore++ : humanScore++;
+function playRound(humanChoice,computerChoice){
+    if(humanChoice == computerChoice){
+        displayRoundResult("tie",computerChoice,humanChoice);
+    }else if(humanChoice == "Rock" && computerChoice == "Paper" || humanChoice == "Paper" && computerChoice == "Scissors" || humanChoice == "Scissors" && computerChoice == "Rock"){
+        computerScore++;
+        displayRoundResult("comp",computerChoice,humanChoice);
     }else{
-        computerGuess == "Rock" ? computerScore++ : humanScore++;
+        humanScore++;
+        displayRoundResult("Human",computerChoice,humanChoice);
     }
+    
+}
+
+function displayRoundResult(winner,computerChoice,humanChoice){
+    if(winner == "tie"){
+        console.log("You both chose " + humanChoice + ", it's a tie");
+    }else{
+    let result;
+        winner == "comp" ? result = "lose" : result = "win";
+        console.log("Computer chose " + computerChoice + " and you chose " + humanChoice + ", you " + result + " this round");
+    }
+    console.log(`Your Score: ${humanScore}  , computer score: ${computerScore}`);
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+playRound(getHumanChoice(),getComputerChoice());
 
 
