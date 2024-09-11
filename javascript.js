02dc32acc8a828bdcf553a1e451a77579b6569c6
adeeb boumoghlbey <adeebboumoghlbey@users.noreply.github.com>
2024-09-11 20:46:03 +0300

    Retailor the javascript for the gui
    add event listeners for the rock, paper, scissors, and new game buttons.
    add code that edits the dom instead of using console.log()
    add code that changes the image links depending on the player and computer choice

--- javascript.js
+++ javascript.js
@@ -1,62 +1,113 @@
 function getComputerChoice(){
     let Choice = Math.floor(Math.random()*3) + 1;
-    if(Choice == 1)return "Rock";
-    else if(Choice == 2)return "Paper";
-    else return "Scissors";
-}
-
-function validChoice(s){
-    return s == "Rock" || s == "Paper" || s == "Scissors";
-}
-
-function getHumanChoice(){
-    let Choice = "l";
-    while(! validChoice(Choice)){
-        Choice = prompt("Enter your Choice(Rock-Paper-Scissors):");
-        Choice = Choice.charAt(0).toUpperCase() + Choice.slice(1).toLowerCase();
+    if(Choice == 1){
+        computerImage.src = "images/rock.png";
+        return "Rock";
+    }else if(Choice == 2){
+        computerImage.src = "images/paper.png";
+        return "Paper";
+    }else{
+        computerImage.src = "images/scissors.png";
+        return "Scissors";
     }
-    return Choice;
 }
+
+// function validChoice(s){
+//     return s == "Rock" || s == "Paper" || s == "Scissors";
+// }
+
+// function getHumanChoice(){
+//     let Choice = "l";
+//     while(! validChoice(Choice)){
+//         Choice = prompt("Enter your Choice(Rock-Paper-Scissors):");
+//         Choice = Choice.charAt(0).toUpperCase() + Choice.slice(1).toLowerCase();
+//     }
+//     return Choice;
+// }
 
 function playRound(humanChoice,computerChoice){
     if(humanChoice == computerChoice){
         displayRoundResult("tie",computerChoice,humanChoice);
     }else if(humanChoice == "Rock" && computerChoice == "Paper" || humanChoice == "Paper" && computerChoice == "Scissors" || humanChoice == "Scissors" && computerChoice == "Rock"){
         computerScore++;
+        computerScoreDisplay.textContent = String(computerScore);
         displayRoundResult("comp",computerChoice,humanChoice);
     }else{
         humanScore++;
+        humanScoreDisplay.textContent = String(humanScore);
         displayRoundResult("Human",computerChoice,humanChoice);
     }
-    
+    if(humanScore == 5 || computerScore == 5){
+        gameOngoing = false;
+        humanScore == 5 ?  resultDisplay.textContent = "You Win!!" : resultDisplay.textContent = "You loose";
+    }
 }
 
 function displayRoundResult(winner,computerChoice,humanChoice){
     if(winner == "tie"){
         console.log("You both chose " + humanChoice + ", it's a tie");
+        resultDisplay.textContent = "Tie";
     }else{
     let result;
-        winner == "comp" ? result = "lose" : result = "win";
+        winner == "comp" ? resultDisplay.textContent = "you lose this round" : resultDisplay.textContent = "you win this round";
         console.log("Computer chose " + computerChoice + " and you chose " + humanChoice + ", you " + result + " this round");
     }
     console.log(`Your Score: ${humanScore}  , computer score: ${computerScore}`);
 }
 
-function playgame(){
-    let i = 0;
-    while(i < 5){
-        let prevCompScore = computerScore;
-        let prevHumanScore = humanScore; 
-        playRound(getHumanChoice(),getComputerChoice());
-        if(prevCompScore != computerScore || prevHumanScore != humanScore)i++;
-    }
-    humanScore > computerScore ? console.log("YOU WIN!!!!") : console.log("You lose :(");
+
+function reset(){
+    humanScore = 0;
+    computerScore = 0;
+    humanImage.src = "images/question-mark.png";
+    computerImage.src = "images/question-mark.png";
+    humanScoreDisplay.textContent = String(humanScore);
+    computerScoreDisplay.textContent = String(humanScore);
+    resultDisplay.textContent = 'New Game';
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-playgame();
+let humanImage = document.querySelector(".player-choice-display > img");
+let computerImage = document.querySelector(".computer-choice-display > img");
+let humanScoreDisplay = document.querySelector(".player-score-display > div:nth-child(2)");
+let computerScoreDisplay = document.querySelector(".computer-score-display > div:nth-child(2)");
+let resultDisplay = document.querySelector(".result-display");
 
+let rockButton = document.querySelector(".rock-button");
+let paperButton = document.querySelector(".paper-button");
+let scissorsButton = document.querySelector(".scissors-button");
+let newGameButton =document.querySelector(".new-game");
 
+let gameOngoing = false;
 
+rockButton.addEventListener('click',() => {
+    if(!gameOngoing){
+        reset();
+        gameOngoing = true;
+    }
+    gameOngoing = true;
+    humanImage.src = "./images/rock.png";
+    playRound("Rock",getComputerChoice());
+});
+
+paperButton.addEventListener('click',() => {
+    if(!gameOngoing){
+        reset();
+        gameOngoing = true;
+    }
+    humanImage.src = "./images/paper.png";
+    playRound("Paper",getComputerChoice());
+});
+
+scissorsButton.addEventListener('click',() =>{
+    if(!gameOngoing){
+        reset();
+        gameOngoing = true;
+    }
+    humanImage.src = "./images/scissors.png";
+    playRound("Scissors",getComputerChoice());
+});
+
+newGameButton.addEventListener("click",reset);
